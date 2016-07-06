44bdb45b056ea3779c4f1f184c73502d045d4963
StefanSinapov <stefan.sinapov@gmail.com>
2016-07-06 08:09:44 +0300

    add all

---
+++ controllers/AccountsController.js
@@ -0,0 +1,22 @@
+﻿moviesApp.controller('AccountsController', function ($scope, $http, $rootScope) {
+    $http.get('json/users.json')
+         .then(function (res) {
+             $scope.accounts = res.data;
+         });
+
+    //IMoviesUIService.GetName(onSuccess,onError);
+
+    var onSuccess = function (result)
+    {
+        $scope.accounts = result.data;
+    }
+
+    var onError = function (result) {
+        $scope.accounts = result.data;
+    }
+    
+    $rootScope.selectUser = function(user) {
+      $rootScope.selectedUser = user;
+      location.href = '/#/profileinfo';
+    };
+});
